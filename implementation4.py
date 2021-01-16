441fcad98f3dd1d79d9a22c65c53ad005087c6e1
mangming2 <jiho402@naver.com>
2021-01-16 14:56:44 +0000

    21-1-16

--- implementation4.py
+++ implementation4.py
@@ -37,4 +37,44 @@ while True:
     break
 
   print(count)
+
+  ''''
+  n,m = map(int,input().split())
+x,y,direction= map(int,input().split())
+
+array=[]
+for i in range(n):
+  array.append(list(map(int,input().split())))
+
+array[x][y]=1
+
+dx=[-1,0,1,0]
+dy=[0,-1,0,1]
+def turn():
+ global direction
+ direction +=1
+ if direction==4:
+   direction=0
+count =1
+turn_count=0
+while True:
+  
+  turn()
+  nx= x+dx[direction]
+  ny= y+dy[direction]
+  if array[nx][ny]==0:
+    x=nx
+    y=ny
+    array[x][y]=1
+    count+=1
+    turn_count=0
+    continue
+  else:
+    
+    turn_count+=1
+  if turn_count==4:
+    break
+
+print(count)
+'''
     
--- main.py
+++ main.py
@@ -1,38 +1,10 @@
-n,m = map(int,input().split())
-x,y,direction =  map(int,input().split())
-array=[]
+def almostIncreasingSequence(sequence):
+    flag = 0
+    for i in range(len(sequence)-1):
+        if sequence[i] >= sequence[i+1]:
+            flag += 1
+                
+    if flag <= 1: return True
+    else: return False
 
-for i in range(n):
-  array.append(list(map(int,input().split())))
-
-dx=[-1,0,1,0]
-dy=[0,1,0,-1]
-array[x][y]=1
-def turn():
-  global direction
-  direction+=1
-  if direction==4:
-    direction=0
-result=1
-turn_count=0
-while True:
-  turn()
-  
-  nx= x+dx[direction]
-  ny = y+dy[direction]
-
-  if array[nx][ny]==0:
-    array[nx][ny]=1
-    result+=1
-    x=nx
-    y=ny
-    turn_count=0
-    continue
-  else:
-    turn_count+=1
-
-  if turn_count==4:
-    break
-
-print(result)
 
